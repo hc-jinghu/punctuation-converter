8eacbec0853457abec429db54b5e9a584c5e00f6
Jing <jhu8187@gmail.com>
2023-09-10 12:42:17 -0400

    feat: fix regex bug

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
-import { Plugin } from 'obsidian';
+import { Notice, Plugin } from 'obsidian';
+
+interface Map {
+	[key: string]:string
+}
 
 export default class Punconvert extends Plugin {
 
-	async onload() {}
+	async onload() {
+		this.addCommand({
+			id: "Convert",
+			name: "Half to full width",
+			editorCallback: (editor, view)=>{	
+				const correction:Map = {
+					",":"，",
+					"?":"？",
+					":":"：",
+					";":"；",
+					"~":"～",
+					".":"．",
+				}
+				let selectedText = editor.getSelection();
+				selectedText = selectedText.replace(/,|\?|:|;|~|\./g, (match) => correction[match]);
+				editor.replaceSelection(selectedText); 
+
+				new Notice("Balala Bao~");
+			}
+		});
+	}
 
 	onunload() {}
 }
